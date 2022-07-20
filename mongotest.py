539c6cf76743c43d4e3f77b40e8daf518cde52fa
Dheerendra Singh Neg <negidheerendra@gmail.com>
2022-07-20 20:38:59 +0530

    this is my first git commit and push operation

---
+++ mongotest.py
@@ -0,0 +1,14 @@
+import pymongo
+
+client = pymongo.MongoClient("mongodb+srv://negidheerendra:ineuron1234@cluster0.hz0k4.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+dict1={
+    "id":1234,
+    "name":"dheerendra",
+    "email":"abc@gmail.com"
+}
+
+conn = client['mongotest']
+coll=conn['test']
+coll.insert_one(dict1)
